d9d1da269a301591bb4ccb65644121feb29db651
Gautam <shagautam@deloitte.com>
2025-06-13 15:26:18 +0530

    navbar update

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SingleProductComponent } from './single-product/single-product.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleProductComponent
+    SingleProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
